b70ca3da5339ecba1be31dbc656e6aeca75518bd
Alexey Ivanov <4985900@gmail.com>
2018-11-14 09:36:16 +0300

    Array of templates is changed from querySelectorAll to querySelector each template. Loop of screens is removed

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 'use strict';
 
-const templatesNodelist = document.querySelectorAll(`template`);
 const main = document.querySelector(`#main`);
 const body = document.body;
+const tmpl = [];
+
+const intro = document.querySelector(`#intro`);
+const greeting = document.querySelector(`#greeting`);
+const rules = document.querySelector(`#rules`);
+const game1 = document.querySelector(`#game-1`);
+const game2 = document.querySelector(`#game-2`);
+const game3 = document.querySelector(`#game-3`);
+const stats = document.querySelector(`#stats`);
+
 let currentScreen = 0;
 
 body.insertAdjacentHTML(`beforeend`, `<div class="arrows__wrap">
@@ -27,7 +36,15 @@ body.insertAdjacentHTML(`beforeend`, `<div class="arrows__wrap">
 const btnLeft = body.querySelector(`.arrows__btn--left`);
 const btnRight = body.querySelector(`.arrows__btn--right`);
 
-const templates = Array.from(templatesNodelist).map((el) => {
+tmpl.push(intro);
+tmpl.push(greeting);
+tmpl.push(rules);
+tmpl.push(game1);
+tmpl.push(game2);
+tmpl.push(game3);
+tmpl.push(stats);
+
+const fixedTmpl = tmpl.map((el) => {
   const shadow = document.createElement(`div`);
   const content = el.content.cloneNode(true);
   shadow.appendChild(content);
@@ -36,29 +53,15 @@ const templates = Array.from(templatesNodelist).map((el) => {
 
 const showScreen = (n) => {
   main.innerHTML = ``;
-  main.appendChild(templates[n]);
-};
-
-const increaseCurrentScreen = () => {
-  currentScreen += 1;
-  if (currentScreen >= templates.length) {
-    currentScreen = 0;
-  }
-};
-
-const decreaseCurrentScreen = () => {
-  currentScreen -= 1;
-  if (currentScreen < 0) {
-    currentScreen = templates.length - 1;
-  }
+  main.appendChild(fixedTmpl[n]);
 };
 
 const onArrowPress = (e) => {
   if (e.key === `ArrowLeft`) {
-    decreaseCurrentScreen();
+    currentScreen -= 1;
   }
   if (e.key === `ArrowRight`) {
-    increaseCurrentScreen();
+    currentScreen += 1;
   }
   showScreen(currentScreen);
 };
@@ -70,10 +73,10 @@ showScreen(currentScreen);
 
 document.addEventListener(`keydown`, onArrowPress);
 btnLeft.addEventListener(`click`, () => {
-  decreaseCurrentScreen();
+  currentScreen -= 1;
   showScreen(currentScreen);
 });
 btnRight.addEventListener(`click`, () => {
-  increaseCurrentScreen();
+  currentScreen += 1;
   showScreen(currentScreen);
 });
