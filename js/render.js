e7da3b53b6a8465217ae9e74f65c53e164c73f0b
Alexey Ivanov <4985900@gmail.com>
2018-11-16 14:17:58 +0300

    render.js created

---
+++ js/render.js
@@ -0,0 +1,9 @@
+// const showScreen = (n) => {
+//   main.innerHTML = ``;
+//   main.appendChild(fixedTmpl[n]);
+// };
+
+const showScreen = (p, e) => {
+  p.innerHTML = ``;
+  p.appendChild(e);
+};
