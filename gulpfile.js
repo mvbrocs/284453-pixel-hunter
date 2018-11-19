33b4d53abd47a123ccaea15ffe687fee40f768e7
Ivanov Alexey <4985900@gmail.com>
2018-11-19 22:32:54 +0300

    chai and mocha added

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,20 @@ const imagemin = require(`gulp-imagemin`);
 const svgstore = require(`gulp-svgstore`);
 const rollup = require(`gulp-better-rollup`);
 const sourcemaps = require(`gulp-sourcemaps`);
+const mocha = require(`gulp-mocha`);
+const commonjs = require(`rollup-plugin-commonjs`);
+
+gulp.task(`test`, () => {
+  return gulp.src([`js/**/*.test.js`])
+    .pipe(rollup({
+      plugins: [
+        commonjs() // Сообщает Rollup, что модули можно загружать из node_modules
+      ]}, `cjs`)) // Выходной формат тестов — `CommonJS` модуль
+    .pipe(gulp.dest(`build/test`))
+    .pipe(mocha({
+      reporter: `spec` // Вид в котором я хочу отображать результаты тестирования
+    }));
+});
 
 gulp.task(`style`, () => {
   return gulp.src(`sass/style.scss`).
