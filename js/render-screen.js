f254d9ade09f5f68e4c5a49ad89f5e28971d62f4
Alexey Ivanov <4985900@gmail.com>
2018-11-28 09:54:02 +0300

    gameScreen object created

--- js/render-screen.js
+++ js/render-screen.js
@@ -8,13 +8,13 @@ import stats from './screens/stats';
 import checkLives from './data/check-lives';
 
 const renderScreen = () => {
-  let lives = checkLives(answers);
-  if (!lives) {
-    return showScreen(stats(gamePlay));
-  }
-  return (gamePlay.GAME_SETUP.level < screens.length)
-    ? showScreen(screens[gamePlay.GAME_SETUP.level])
-    : showScreen(stats(gamePlay));
+  // let lives = checkLives(answers);
+  // if (!lives) {
+  //   return showScreen(stats(gamePlay));
+  // }
+  // return (gamePlay.GAME_SETUP.level < screens.length)
+  //   ? showScreen(screens[gamePlay.GAME_SETUP.level])
+  //   : showScreen(stats(gamePlay));
 };
 
 export default renderScreen;
