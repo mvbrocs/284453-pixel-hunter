e27cfb2aac04a1c7499f4748e7b6feae9e0a37a5
Alexey Ivanov <4985900@gmail.com>
2018-11-30 11:17:53 +0300

    reset game in greeting screen

--- js/screens/greeting.js
+++ js/screens/greeting.js
@@ -3,6 +3,7 @@ import {
   showScreen
 } from "../utils";
 import rules from "./rules";
+import { gameState } from "../data/game-state";
 
 const greeting = () => {
   const greetingHtml = `
@@ -29,6 +30,8 @@ const greeting = () => {
 
   const greetingEl = makeElement(greetingHtml);
   const arrowRight = greetingEl.querySelector(`.greeting__continue`);
+
+  gameState.resetGame();
 
   arrowRight.addEventListener(`click`, () => showScreen(rules()));
   return greetingEl;
