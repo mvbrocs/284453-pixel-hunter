97b26278d1c879994cf39ccc2f7f0deddfc19280
Alexey Ivanov <4985900@gmail.com>
2018-11-30 11:19:48 +0300

    module is disabled

--- js/start-game.js
+++ js/start-game.js
@@ -1,14 +1,21 @@
-import {
-  game
-} from './switch-screens';
-import {
-  gameState
-} from './data/game-state';
+// // import {
+// //   game
+// // } from './switch-screens';
+// import {
+//   gameState
+// } from './data/game-state';
+// import game1 from './screens/game-1';
+// import { showScreen } from './utils';
 
 
-const startGame = () => {
-  gameState.resetGame();
-  gameState.showScreenWithData(gameState.getState(), game);
-};
+// const startGame = () => {
+//   // gameState.resetGame();
+//   // gameState.showScreenWithData(gameState.getState());
+//   // debugger;
+//   gameState.addAnswer(true, 333);
+//   const state = gameState.getState();
+// 	console.log("​startGame -> state", state);
+//   // showScreen(game1(gameState.getState()));
+// };
 
-export default startGame;
+// export default startGame;
