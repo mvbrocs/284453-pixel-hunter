877252837ca44f54f1ec16f5ee5d318adbcc8170
Ivanov Alexey <4985900@gmail.com>
2018-12-02 22:05:04 +0300

    addede comments & render()

--- js/view/abstract-view.js
+++ js/view/abstract-view.js
@@ -1,8 +1,22 @@
 class AbstractView {
-  get template() {}
-  get element() {}
-  render() {}
-  bind() {}
+  get template() {
+    // Возвращает строку, содержащую разметку
+    // переопределяется в наследнике
+  }
+  get element() {
+    // ?????????????
+    // Возвращает DOM-элемент, соответствующий представлению
+  }
+  render(str) {
+    // создает DOM элемент на основе шаблона геттера template
+    const div = document.createElement(`div`);
+    div.innerHTML = str.trim();
+    return div;
+  }
+  bind() {
+    // добавляет обработчики событий
+    // переопределяется в наследнике
+  }
 }
 
 export default AbstractView;
