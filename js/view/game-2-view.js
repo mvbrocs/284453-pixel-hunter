2a362410e4c7c6df550a8b61a4f81de3aacaa245
Alexey Ivanov <4985900@gmail.com>
2018-12-05 14:32:06 +0300

    game2 class added

--- js/view/game-2-view.js
+++ js/view/game-2-view.js
@@ -1,19 +1,61 @@
-import Game2 from "../view/game-2-view";
+import AbstractView from './abstract-view';
 import {
-  gameState
-} from "../data/game-state";
-import greeting from "./greeting";
-import {
-  showScreen
-} from "../utils/utils";
+  BackButton,
+  Timer,
+  Lives
+} from './header-view';
 
-const game2 = new Game2(gameState);
-game2.onBackButtonClick = () => {
-  showScreen(greeting.element);
-};
+export default class Game2 extends AbstractView {
+  constructor(data) {
+    super();
+    this.data = data;
+    this.state = data.getState();
+  }
+  template() {
+    const gameTask = `<p class="game__task">${this.state.gameScreens[this.state.level].question}</p>`;
 
-export default game2;
+    return `
+      <header class="header">
+        ${new BackButton().template}
+        ${new Timer(this.state.time).template}
+        ${new Lives(this.state.lives).template}
+      </header>
+      <section class="game">
+        ${gameTask}
+        <form class="game__content  game__content--wide">
+          <div class="game__option">
+            <img src="${this.state.gameScreens[this.state.level].answers[0].image.url}" alt="Option 1" width="705" height="455">
+            <label class="game__answer  game__answer--photo">
+              <input class="visually-hidden" name="question1" type="radio" value="photo">
+              <span>Фото</span>
+            </label>
+            <label class="game__answer  game__answer--paint">
+              <input class="visually-hidden" name="question1" type="radio" value="paint">
+              <span>Рисунок</span>
+            </label>
+          </div>
+        </form>
+      </section>`;
+  }
+  bind() {
+    const form = this.element.querySelector(`.game__content`);
+    const backButton = this.element.querySelector(`.back`);
+    const gameSection = this.element.querySelector(`.game`);
 
+    // gameSection.appendChild(statsTemplate(data));
+
+    form.addEventListener(`change`, () => {
+      this.onFormChange();
+    });
+
+    backButton.addEventListener(`click`, (e) => {
+      e.preventDefault();
+      this.onBackButtonClick();
+    });
+  }
+  onFormChange() {}
+  onBackButtonClick() {}
+}
 
 // import {
 //   makeElement,
