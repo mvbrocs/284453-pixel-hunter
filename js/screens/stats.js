2315224547016b1afffd1108afdd312570f7c8a4
Alexey Ivanov <4985900@gmail.com>
2018-12-07 11:28:19 +0300

    stats screen is ready

--- js/screens/stats.js
+++ js/screens/stats.js
@@ -1,7 +1,20 @@
 import Stats from "../view/stats-view";
+import gameState from "../data/game-state";
+import {
+  showScreen
+} from "../utils/utils";
+import greeting from "./greeting";
+import statsBar from "./stats-bar";
 
 export default () => {
-  const stats = new Stats();
+  const stats = new Stats(gameState, gameState.getState());
+  stats.onBackButtonClick = () => {
+    showScreen(greeting().element);
+  };
+  stats.insertStatsBar = () => {
+    return statsBar(gameState.getState()).template;
+  };
+
   return stats;
 };
 
--- js/view/stats-view.js
+++ js/view/stats-view.js
@@ -2,17 +2,88 @@ import AbstractView from './abstract-view';
 import {
   BackButton
 } from './header-view';
+import {
+  Answer
+} from '../data/game-data';
 
 export default class Stats extends AbstractView {
-  constructor(data) {
+  constructor(data, state) {
     super();
     this.data = data;
+    this.state = state;
+    this.totalScores = this.data.totalScores(this.state);
   }
   get template() {
-    return `
-      <b>Привет!</b>
-    `;
+    const quickAnswersTotal = this.data.quickAnswersCount(this.state);
+    const slowAnswersTotal = this.data.slowAnswersCount(this.state);
+    const correctAnswersTotal = this.data.correctAnswersCount(this.state);
+
+    const getBonusHtml = () => {
+      const bonusesHtml = `
+          <tr>
+            <td></td>
+            <td class="result__extra">Бонус за скорость:</td>
+            <td class="result__extra">${quickAnswersTotal} <span class="stats__result stats__result--fast"></span></td>
+            <td class="result__points">× ${Answer.QUICK}</td>
+            <td class="result__total">${quickAnswersTotal * Answer.QUICK}</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td class="result__extra">Бонус за жизни:</td>
+            <td class="result__extra">${this.state.lives} <span class="stats__result stats__result--alive"></span></td>
+            <td class="result__points">× ${Answer.BONUS_FOR_LIVES}</td>
+            <td class="result__total">${this.state.lives * Answer.BONUS_FOR_LIVES}</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td class="result__extra">Штраф за медлительность:</td>
+            <td class="result__extra">${slowAnswersTotal} <span class="stats__result stats__result--slow"></span></td>
+            <td class="result__points">× ${Answer.SLOW}</td>
+            <td class="result__total">-${slowAnswersTotal * Answer.SLOW}</td>
+          </tr>`;
+
+      if (this.totalScores) {
+        return bonusesHtml;
+      }
+      return ``;
+    };
+
+    const result = () => {
+      return (this.totalScores > 0) ? this.totalScores : false;
+    };
+
+    const statsHtml = `
+      <header class="header">
+        ${new BackButton().template}
+      </header>
+      <section class="result">
+        <h2 class="result__title">Победа!</h2>
+        <table class="result__table">
+          <tr>
+            <td class="result__number">1.</td>
+            <td colspan="2" class="result__stats">${this.insertStatsBar()}</td>
+            <td class="result__points">× ${Answer.RIGHT}</td>
+            <td class="result__total">${correctAnswersTotal * Answer.RIGHT}</td>
+          </tr>
+          ${getBonusHtml()}
+          <tr>
+            <td colspan="5" class="result__total  result__total--final">${result()}</td>
+          </tr>
+        </table>
+      </section>`;
+
+    return statsHtml;
   }
+  bind() {
+    const backButton = this.element.querySelector(`.back`);
+
+    backButton.addEventListener(`click`, (e) => {
+      e.preventDefault();
+      this.onBackButtonClick();
+    });
+  }
+  onBackButtonClick() {}
+  insertStatsBar() {}
 }
 
 // import {
