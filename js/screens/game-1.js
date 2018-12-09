2ea02b7d55bc28cd1e9169e68d6fad6d64659e95
Alexey Ivanov <4985900@gmail.com>
2018-12-09 18:07:16 +0300

    console.log removed

--- js/screens/game-1.js
+++ js/screens/game-1.js
@@ -11,7 +11,6 @@ export default () => {
     showScreen(greeting().element);
   };
   game1.compareChecking = () => {
-		console.log("​game1.compareChecking -> game1.answer", game1.result);
     gameState.addAnswer(game1.result, 1500);
     gameState.checkLivesCount(gameState.getState());
     gameState.changeGameLevel();
