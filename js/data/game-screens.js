baa69aa1c3e2570b11579de2bb51d50121f96650
Alexey Ivanov <4985900@gmail.com>
2018-12-10 16:13:26 +0300

    game data file added

---
+++ js/data/game-screens.js
@@ -0,0 +1,140 @@
+const GAME_SCREENS = [{
+  type: `two-of-two`,
+  question: `Угадайте для каждого изображения фото или рисунок?`,
+  answers: [{
+    image: {
+      url: `http://i.imgur.com/DKR1HtB.jpg`
+    },
+    type: `photo`
+  },
+  {
+    image: {
+      url: `https://k42.kn3.net/CF42609C8.jpg`
+    },
+    type: `photo`
+  }
+  ]
+}, {
+  type: `tinder-like`,
+  question: `Угадай, фото или рисунок?`,
+  answers: [{
+    image: {
+      url: `https://k42.kn3.net/CF42609C8.jpg`
+    },
+    type: `painting`
+  }]
+}, {
+  type: `tinder-like`,
+  question: `Угадай, фото или рисунок?`,
+  answers: [{
+    image: {
+      url: `http://i.imgur.com/1KegWPz.jpg`
+    },
+    type: `photo`
+  }]
+}, {
+  type: `one-of-three`,
+  question: `Найдите рисунок среди изображений`,
+  answers: [{
+    image: {
+      url: `https://i.imgur.com/DiHM5Zb.jpg`
+    },
+    type: `painting`
+  },
+  {
+    image: {
+      url: `http://i.imgur.com/DKR1HtB.jpg`
+    },
+    type: `painting`
+  },
+  {
+    image: {
+      url: `https://k42.kn3.net/CF42609C8.jpg`
+    },
+    type: `painting`
+  }
+  ]
+}, {
+  type: `tinder-like`,
+  question: `Угадай, фото или рисунок?`,
+  answers: [{
+    image: {
+      url: `https://k42.kn3.net/CF42609C8.jpg`
+    },
+    type: `painting`
+  }]
+}, {
+  type: `one-of-three`,
+  question: `Найдите рисунок среди изображений`,
+  answers: [{
+    image: {
+      url: `https://k42.kn3.net/CF42609C8.jpg`
+    },
+    type: `painting`
+  },
+  {
+    image: {
+      url: `https://k42.kn3.net/D2F0370D6.jpg`
+    },
+    type: `painting`
+  },
+  {
+    image: {
+      url: `https://k42.kn3.net/CF42609C8.jpg`
+    },
+    type: `painting`
+  }
+  ]
+}, {
+  type: `two-of-two`,
+  question: `Угадайте для каждого изображения фото или рисунок?`,
+  answers: [{
+    image: {
+      url: `http://i.imgur.com/DKR1HtB.jpg`
+    },
+    type: `photo`
+  },
+  {
+    image: {
+      url: `https://k42.kn3.net/CF42609C8.jpg`
+    },
+    type: `photo`
+  }
+  ]
+}, {
+  type: `tinder-like`,
+  question: `Угадай, фото или рисунок?`,
+  answers: [{
+    image: {
+      url: `https://k42.kn3.net/CF42609C8.jpg`
+    },
+    type: `painting`
+  }]
+}, {
+  type: `two-of-two`,
+  question: `Угадайте для каждого изображения фото или рисунок?`,
+  answers: [{
+    image: {
+      url: `https://i.imgur.com/DiHM5Zb.jpg`
+    },
+    type: `photo`
+  },
+  {
+    image: {
+      url: `https://k42.kn3.net/CF42609C8.jpg`
+    },
+    type: `photo`
+  }
+  ]
+}, {
+  type: `tinder-like`,
+  question: `Угадай, фото или рисунок?`,
+  answers: [{
+    image: {
+      url: `http://i.imgur.com/1KegWPz.jpg`
+    },
+    type: `painting`
+  }]
+}];
+
+export default GAME_SCREENS;
