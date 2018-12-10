5e11b33d96d5271f1d7be2916022cef7b0643710
Alexey Ivanov <4985900@gmail.com>
2018-12-10 16:41:13 +0300

    root added

--- js/controller/game-screen.js
+++ js/controller/game-screen.js
@@ -1,6 +1,17 @@
 export default class GameScreen {
-  constructor() {
+  constructor(model) {
+    this.model = model;
+    // this.header = 
 
+    this.root = document.createElement(`div`);
+
+
+    this._timer = null;
   }
+
+  get element() {
+    return this.root;
+  }
+
   init() {}
 }
