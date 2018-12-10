8838886f4650cdaba9a300b7852d85655fa505e5
Alexey Ivanov <4985900@gmail.com>
2018-12-10 14:58:52 +0300

    changed to Router.showIntro

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-import {
-  showScreen
-} from "./utils/utils";
-import intro from "./screens/intro";
+// import {
+//   showScreen
+// } from "./utils/utils";
+// import intro from "./screens/intro";
+import Router from "./router/application-router";
 
-showScreen(intro().element);
+// showScreen(intro().element);
+Router.showIntro();
