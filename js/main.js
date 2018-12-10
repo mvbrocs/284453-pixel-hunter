bdc1bb7aae6a47daff696ee201d4d1288993bb5b
Alexey Ivanov <4985900@gmail.com>
2018-12-10 14:59:10 +0300

    comments removed

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-// import {
-//   showScreen
-// } from "./utils/utils";
-// import intro from "./screens/intro";
 import Router from "./router/application-router";
 
-// showScreen(intro().element);
 Router.showIntro();
