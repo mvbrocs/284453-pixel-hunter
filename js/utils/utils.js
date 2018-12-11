f7fc78b995425da7857f33fda0ff81a388765824
Alexey Ivanov <4985900@gmail.com>
2018-12-11 11:50:32 +0300

    unused code was commented

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -1,18 +1,19 @@
 const main = document.querySelector(`#main`);
 
-const makeElement = (str) => {
+/* const makeElement = (str) => {
   const div = document.createElement(`div`);
   div.innerHTML = str.trim();
   return div;
 };
-
+ */
 const showScreen = (e) => {
+	console.log("​showScreen -> e", e);
   main.innerHTML = ``;
   main.appendChild(e);
 };
 
 export {
   main,
-  makeElement,
+  // makeElement,
   showScreen
 };
