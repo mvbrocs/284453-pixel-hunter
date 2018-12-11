feae614122cd4205bb21a5b59e426266a62c6542
Alexey Ivanov <4985900@gmail.com>
2018-12-11 16:49:31 +0300

    onBackButtonClick added

--- js/controller/rules-screen.js
+++ js/controller/rules-screen.js
@@ -5,6 +5,7 @@ export default class RulesScreen {
   get element() {
     const rulesScreen = new Rules();
     rulesScreen.onButtonClick = () => Router.showGame();
+    rulesScreen.onBackButtonClick = () => Router.showGreeting();
 
     return rulesScreen.element;
   }
