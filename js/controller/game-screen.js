70b1e605a5813957e6a9e72a01cd13b302ef2186
Alexey Ivanov <4985900@gmail.com>
2018-12-11 09:24:21 +0300

    init() and changeLevel() added

--- js/controller/game-screen.js
+++ js/controller/game-screen.js
@@ -1,17 +1,19 @@
 export default class GameScreen {
   constructor(model) {
     this.model = model;
-    // this.header = 
-
-    this.root = document.createElement(`div`);
-
-
-    this._timer = null;
   }
 
   get element() {
     return this.root;
   }
 
-  init() {}
+  init() {
+    this.model.resetGame();
+  }
+  changeLevel() {
+    // добавляем ответ в массив
+    this.model.checkLivesCount();
+    this.model.changeGameLevel();
+    this.model.checkGameOver();
+  }
 }
