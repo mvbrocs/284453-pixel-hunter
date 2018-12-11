351fcfa465729745fc20aaacfc628832ac330bfd
Alexey Ivanov <4985900@gmail.com>
2018-12-11 15:45:09 +0300

    unused code removed

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -1,19 +1,11 @@
 const main = document.querySelector(`#main`);
 
-/* const makeElement = (str) => {
-  const div = document.createElement(`div`);
-  div.innerHTML = str.trim();
-  return div;
-};
- */
 const showScreen = (e) => {
-	console.log("​showScreen -> e", e);
   main.innerHTML = ``;
   main.appendChild(e);
 };
 
 export {
   main,
-  // makeElement,
   showScreen
 };
