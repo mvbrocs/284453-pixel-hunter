4fda0b57dcdb5ca2ac5577ff4e8b70d987261e28
Ivanov Alexey <4985900@gmail.com>
2018-12-17 22:35:24 +0300

    comments removed

--- js/view/stats-view.js
+++ js/view/stats-view.js
@@ -80,71 +80,71 @@ export default class Stats extends AbstractView {
 
   get template() {
 
-    // const tableHtml = [];
+    const tableHtml = [];
 
-    // this.state.playedGames.forEach((el) => {
-    //   const getBonusHtml = () => {
-    //     const bonusesHtml = `
-    //         <tr>
-    //           <td></td>
-    //           <td class="result__extra">Бонус за скорость:</td>
-    //           <td class="result__extra">${el.quickAnswersTotal} <span class="stats__result stats__result--fast"></span></td>
-    //           <td class="result__points">× ${Answer.QUICK}</td>
-    //           <td class="result__total">${el.quickAnswersTotal * Answer.QUICK}</td>
-    //         </tr>
-    //         <tr>
-    //           <td></td>
-    //           <td class="result__extra">Бонус за жизни:</td>
-    //           <td class="result__extra">${el.livesTotal} <span class="stats__result stats__result--alive"></span></td>
-    //           <td class="result__points">× ${Answer.BONUS_FOR_LIVES}</td>
-    //           <td class="result__total">${el.livesTotal * Answer.BONUS_FOR_LIVES}</td>
-    //         </tr>
-    //         <tr>
-    //           <td></td>
-    //           <td class="result__extra">Штраф за медлительность:</td>
-    //           <td class="result__extra">${el.slowAnswersTotal} <span class="stats__result stats__result--slow"></span></td>
-    //           <td class="result__points">× ${Answer.SLOW}</td>
-    //           <td class="result__total">-${el.slowAnswersTotal * Answer.SLOW}</td>
-    //         </tr>`;
+    this.state.playedGames.forEach((el) => {
+      const getBonusHtml = () => {
+        const bonusesHtml = `
+            <tr>
+              <td></td>
+              <td class="result__extra">Бонус за скорость:</td>
+              <td class="result__extra">${el.quickAnswersTotal} <span class="stats__result stats__result--fast"></span></td>
+              <td class="result__points">× ${Answer.QUICK}</td>
+              <td class="result__total">${el.quickAnswersTotal * Answer.QUICK}</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td class="result__extra">Бонус за жизни:</td>
+              <td class="result__extra">${el.livesTotal} <span class="stats__result stats__result--alive"></span></td>
+              <td class="result__points">× ${Answer.BONUS_FOR_LIVES}</td>
+              <td class="result__total">${el.livesTotal * Answer.BONUS_FOR_LIVES}</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td class="result__extra">Штраф за медлительность:</td>
+              <td class="result__extra">${el.slowAnswersTotal} <span class="stats__result stats__result--slow"></span></td>
+              <td class="result__points">× ${Answer.SLOW}</td>
+              <td class="result__total">-${el.slowAnswersTotal * Answer.SLOW}</td>
+            </tr>`;
 
-    //     if (el.totalScores > 0) {
-    //       return bonusesHtml;
-    //     } else {
-    //       return ``;
-    //     }
-    //   };
+        if (el.totalScores > 0) {
+          return bonusesHtml;
+        } else {
+          return ``;
+        }
+      };
 
-    //   const result = () => {
-    //     return (el.totalScores > 0) ? el.totalScores : false;
-    //   };
+      const result = () => {
+        return (el.totalScores > 0) ? el.totalScores : false;
+      };
 
-    //   const tableContent = `
-    //   <tr>
-    //       <td class="result__number">${this.state.playedGames.indexOf(el) + 1}</td>
-    //       <td colspan="2" class="result__stats">${el.statsBarHtml}</td>
-    //       <td class="result__points">× ${Answer.RIGHT}</td>
-    //       <td class="result__total">${el.correctAnswersTotal * Answer.RIGHT}</td>
-    //     </tr>
-    //       ${getBonusHtml()}
-    //     <tr>
-    //       <td colspan="5" class="result__total  result__total--final">${result()}</td>
-    //     </tr>`;
+      const tableContent = `
+      <tr>
+          <td class="result__number">${this.state.playedGames.indexOf(el) + 1}</td>
+          <td colspan="2" class="result__stats">${el.statsBarHtml}</td>
+          <td class="result__points">× ${Answer.RIGHT}</td>
+          <td class="result__total">${el.correctAnswersTotal * Answer.RIGHT}</td>
+        </tr>
+          ${getBonusHtml()}
+        <tr>
+          <td colspan="5" class="result__total  result__total--final">${result()}</td>
+        </tr>`;
 
-    //   tableHtml.push(tableContent);
-    // });
+      tableHtml.push(tableContent);
+    });
 
-    // const statsHtml = `
-    //   <header class="header">
-    //     ${new BackButton().template}
-    //   </header>
-    //   <section class="result">
-    //     <h2 class="result__title">Победа!</h2>
-    //     <table class="result__table">
-    //      ${tableHtml.join(``)}
-    //     </table>
-    //   </section>`;
+    const statsHtml = `
+      <header class="header">
+        ${new BackButton().template}
+      </header>
+      <section class="result">
+        <h2 class="result__title">Победа!</h2>
+        <table class="result__table">
+         ${tableHtml.join(``)}
+        </table>
+      </section>`;
 
-    // return statsHtml;
+    return statsHtml;
   }
 
   bind() {
