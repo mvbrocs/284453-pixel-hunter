9a3af6794fded2c6facd07e55ad29ae6b875f531
Ivanov Alexey <4985900@gmail.com>
2018-12-17 22:16:00 +0300

    saving and loadnig data was added

---
+++ js/utils/loader.js
@@ -0,0 +1,41 @@
+const SERVER_URL = `https://es.dump.academy/pixel-hunter/`;
+const APP_ID = 123321;
+
+const checkStatus = (response) => {
+  if (response.ok) {
+    return response;
+  } else {
+    throw new Error(`${response.status}: ${response.statusText}`);
+  }
+};
+
+const toJSON = (res) => res.json();
+
+class Loader {
+  static loadData() {
+    return fetch(`${SERVER_URL}/questions`).
+    then(checkStatus).
+    then(toJSON);
+  }
+
+  static loadResults(name) {
+    return fetch(`${SERVER_URL}/stats/:${APP_ID}-:${name}`).
+    then(checkStatus).
+    then(toJSON);
+  }
+
+  static saveResults(data) {
+    const requestSettings = {
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': `application/json`
+      },
+      method: `POST`
+    };
+
+    return fetch(`${SERVER_URL}/stats/:${APP_ID}-:${name}`, requestSettings).
+    then(checkStatus);
+  }
+}
+
+export default Loader;
