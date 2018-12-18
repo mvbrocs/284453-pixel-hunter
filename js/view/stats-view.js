ff90c605aaa48af77523302ee6921a830a909880
Alexey Ivanov <4985900@gmail.com>
2018-12-18 13:09:58 +0300

    fixed

--- js/view/stats-view.js
+++ js/view/stats-view.js
@@ -16,52 +16,48 @@ export default class Stats extends AbstractView {
     this.state.forEach((el) => {
       const getBonusHtml = () => {
         const bonusesHtml = `
-            <tr>
-              <td></td>
-              <td class="result__extra">Бонус за скорость:</td>
-              <td class="result__extra">${el.quickAnswersTotal} <span class="stats__result stats__result--fast"></span></td>
-              <td class="result__points">× ${Answer.QUICK}</td>
-              <td class="result__total">${el.quickAnswersTotal * Answer.QUICK}</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td class="result__extra">Бонус за жизни:</td>
-              <td class="result__extra">${el.livesTotal} <span class="stats__result stats__result--alive"></span></td>
-              <td class="result__points">× ${Answer.BONUS_FOR_LIVES}</td>
-              <td class="result__total">${el.livesTotal * Answer.BONUS_FOR_LIVES}</td>
-            </tr>
-            <tr>
-              <td></td>
-              <td class="result__extra">Штраф за медлительность:</td>
-              <td class="result__extra">${el.slowAnswersTotal} <span class="stats__result stats__result--slow"></span></td>
-              <td class="result__points">× ${Answer.SLOW}</td>
-              <td class="result__total">-${el.slowAnswersTotal * Answer.SLOW}</td>
-            </tr>`;
+          <tr>
+            <td class="result__number">${this.state.indexOf(el) + 1}</td>
+            <td colspan="2" class="result__stats">${el.statsBarHtml}</td>
+            <td class="result__points">× ${Answer.RIGHT}</td>
+            <td class="result__total">${el.correctAnswersTotal * Answer.RIGHT}</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td class="result__extra">Бонус за скорость:</td>
+            <td class="result__extra">${el.quickAnswersTotal} <span class="stats__result stats__result--fast"></span></td>
+            <td class="result__points">× ${Answer.QUICK}</td>
+            <td class="result__total">${el.quickAnswersTotal * Answer.QUICK}</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td class="result__extra">Бонус за жизни:</td>
+            <td class="result__extra">${el.livesTotal} <span class="stats__result stats__result--alive"></span></td>
+            <td class="result__points">× ${Answer.BONUS_FOR_LIVES}</td>
+            <td class="result__total">${el.livesTotal * Answer.BONUS_FOR_LIVES}</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td class="result__extra">Штраф за медлительность:</td>
+            <td class="result__extra">${el.slowAnswersTotal} <span class="stats__result stats__result--slow"></span></td>
+            <td class="result__points">× ${Answer.SLOW}</td>
+            <td class="result__total">-${el.slowAnswersTotal * Answer.SLOW}</td>
+          </tr>
+          <tr>
+            <td colspan="5" class="result__total  result__total--final">${el.totalScores}</td>
+          </tr>`;
 
-        if (el.totalScores > 0) {
-          return bonusesHtml;
-        } else {
-          return ``;
-        }
+        const failHtml = `
+          <tr>
+            <td class="result__number">${this.state.indexOf(el) + 1}</td>
+            <td colspan="2" class="result__stats">${el.statsBarHtml}</td>
+            <td class="result__total">FAIL</td>
+          </tr>`;
+
+        return (el.totalScores > 0) ? bonusesHtml : failHtml;
       };
 
-      const result = () => {
-        return (el.totalScores > 0) ? el.totalScores : false;
-      };
-
-      const tableContent = `
-      <tr>
-          <td class="result__number">${this.state.indexOf(el) + 1}</td>
-          <td colspan="2" class="result__stats">${el.statsBarHtml}</td>
-          <td class="result__points">× ${Answer.RIGHT}</td>
-          <td class="result__total">${el.correctAnswersTotal * Answer.RIGHT}</td>
-        </tr>
-          ${getBonusHtml()}
-        <tr>
-          <td colspan="5" class="result__total  result__total--final">${result()}</td>
-        </tr>`;
-
-      tableHtml.push(tableContent);
+      tableHtml.push(getBonusHtml());
     });
 
     const statsHtml = `
@@ -69,13 +65,17 @@ export default class Stats extends AbstractView {
         ${new BackButton().template}
       </header>
       <section class="result">
-        <h2 class="result__title">Победа!</h2>
+        <h2 class="result__title">${this.getHeader(this.state)}!</h2>
         <table class="result__table">
          ${tableHtml.join(``)}
         </table>
       </section>`;
 
     return statsHtml;
+  }
+
+  getHeader(state) {
+    return (state[state.length - 1].totalScores > 0) ? `Победа` : `Поражение`;
   }
 
   bind() {
